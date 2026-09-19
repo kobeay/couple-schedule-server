e51c525804e6e5f0f8355c3b410ae6203d331a18
kobeay <rhaudwns4981@naver.com>
2026-09-19 22:19:09 +0900

    chore: initialize Spring Boot project with MySQL

---
+++ src/main/java/com/kobeay/couple_schedule/server/CoupleScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package com.kobeay.couple_schedule.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleScheduleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobeay/couple_schedule/server/CoupleScheduleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobeay.couple_schedule.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupleScheduleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
