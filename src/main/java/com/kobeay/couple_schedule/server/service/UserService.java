891386f752ed6e3e96233476c155c75a52b99d77
kobeay <rhaudwns4981@naver.com>
2026-09-21 14:02:16 +0900

    feat: User Service 구현 (#3)

---
+++ src/main/java/com/kobeay/couple_schedule/server/service/UserService.java
@@ -0,0 +1,24 @@
+package com.kobeay.couple_schedule.server.service;
+
+import com.kobeay.couple_schedule.server.entity.User;
+import com.kobeay.couple_schedule.server.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
